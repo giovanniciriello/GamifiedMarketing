c48a8d9e7d67b626658db95c72c5f709a556e0fe
zTehRyaN <MatteoGio96@gmail.com>
2020-11-12 12:55:13 +0100

    (code) Small improvements

--- gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/repository/UserRepository.java
+++ gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/repository/UserRepository.java
@@ -3,6 +3,9 @@ package it.polimi.db2.gamifiedmarketing.repository;
 import it.polimi.db2.gamifiedmarketing.entity.User;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
+
+@Transactional
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findUserById(Integer id);
