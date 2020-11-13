e0076949709fe4d9c3acaa7d29d3acb1673805ef
zTehRyaN <MatteoGio96@gmail.com>
2020-11-13 10:40:53 +0100

    (entitiy) Product entity sketch

--- gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/entity/Product.java
+++ gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/entity/Product.java
@@ -21,4 +21,36 @@ public class Product {
 
     @NotNull
     private String imageUrl;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
