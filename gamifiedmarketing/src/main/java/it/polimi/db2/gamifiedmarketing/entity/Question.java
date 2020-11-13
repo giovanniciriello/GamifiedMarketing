5dd042c5048f70720922f253d0469a61b6acfc20
zTehRyaN <MatteoGio96@gmail.com>
2020-11-13 10:42:42 +0100

    (entitiy) Question entity sketch

---
+++ gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/entity/Question.java
@@ -0,0 +1,45 @@
+package it.polimi.db2.gamifiedmarketing.entity;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Question {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    private String title;
+
+    private String subtitle;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+}
