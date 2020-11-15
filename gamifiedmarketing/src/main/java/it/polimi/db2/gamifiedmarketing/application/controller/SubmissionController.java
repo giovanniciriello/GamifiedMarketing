77e22bff7740a4cab022147e101724200f9837db
zTehRyaN <MatteoGio96@gmail.com>
2020-11-15 16:21:33 +0100

    (controller) Added new controller for submission

---
+++ gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/controller/SubmissionController.java
@@ -0,0 +1,7 @@
+package it.polimi.db2.gamifiedmarketing.application.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SubmissionController {
+}
--- gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/service/SubmissionService.java
+++ gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/service/SubmissionService.java
@@ -1,7 +1,12 @@
 package it.polimi.db2.gamifiedmarketing.application.service;
 
+import it.polimi.db2.gamifiedmarketing.application.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SubmissionService {
+
+    @Autowired
+    private UserRepository userRepository;
 }
