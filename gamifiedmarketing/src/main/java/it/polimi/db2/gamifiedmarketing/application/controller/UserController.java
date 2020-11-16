2ea317c4aa1344aaec2d9873ccb2c61f3bd659bd
Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
2020-11-16 12:43:17 +0100

    bootstrap test

--- gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/controller/UserController.java
+++ gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/controller/UserController.java
@@ -3,13 +3,21 @@ package it.polimi.db2.gamifiedmarketing.application.controller;
 import it.polimi.db2.gamifiedmarketing.application.entity.User;
 import it.polimi.db2.gamifiedmarketing.application.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+
+    @GetMapping("/test")
+    public String index(Model model) {
+        model.addAttribute("greeting", "Giovanni");
+        return "home";
+    }
 
     @PostMapping("/add")
     public String addUser(@RequestParam String first, @RequestParam String last) {
