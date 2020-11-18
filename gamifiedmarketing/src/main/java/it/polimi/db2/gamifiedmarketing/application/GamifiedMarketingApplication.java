0264d5436decd904b92907fc0b1e160c2c7afa87
zTehRyaN <MatteoGio96@gmail.com>
2020-11-18 14:28:19 +0100

    (app) User - Admin - Product initial DB fill

--- gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/GamifiedMarketingApplication.java
+++ gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/GamifiedMarketingApplication.java
@@ -1,20 +1,25 @@
 package it.polimi.db2.gamifiedmarketing.application;
 
+import it.polimi.db2.gamifiedmarketing.application.entity.Product;
 import it.polimi.db2.gamifiedmarketing.application.entity.User;
-import it.polimi.db2.gamifiedmarketing.application.entity.enums.UserRole;
+import it.polimi.db2.gamifiedmarketing.application.repository.ProductRepository;
 import it.polimi.db2.gamifiedmarketing.application.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.time.Month;
 
 @SpringBootApplication
 public class GamifiedMarketingApplication {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(GamifiedMarketingApplication.class, args);
@@ -28,12 +33,26 @@ public class GamifiedMarketingApplication {
          */
 
         // --- Users --- \\
-        User user1 = User.builder().firstName("Matteo").lastName("Giordano").email("matteo@email.com").password("pwd").role(UserRole.CUSTOMER).build();
-        User user2 = User.builder().firstName("Andrea").lastName("Bovo").email("andrea@email.com").password("pwd").role(UserRole.CUSTOMER).build();
-        User user3 = User.builder().firstName("Giovanni").lastName("Ciriello").email("giovanni@email.com").password("pwd").role(UserRole.CUSTOMER).build();
+        User user1 = User.builder().firstName("Matteo").lastName("Giordano").email("matteo@email.com").password("pwd").isAdmin(false).build();
+        User user2 = User.builder().firstName("Andrea").lastName("Bovo").email("andrea@email.com").password("pwd").isAdmin(false).build();
+        User user3 = User.builder().firstName("Giovanni").lastName("Ciriello").email("giovanni@email.com").password("pwd").isAdmin(false).build();
         userRepository.save(user1);
         userRepository.save(user2);
         userRepository.save(user3);
+
+        // --- Admin --- \\\
+        User admin1 = User.builder().firstName("Admin").lastName("1").email("admin1@email.com").password("pwd").isAdmin(true).build();
+        User admin2 = User.builder().firstName("Admin").lastName("2").email("admin2@email.com").password("pwd").isAdmin(true).build();
+        userRepository.save(admin1);
+        userRepository.save(admin2);
+
+        // --- Products --- \\\
+        Product product1 = Product.builder().name("First").description("The first product").imageUrl("Url1").date(LocalDate.of(2020, Month.NOVEMBER, 17)).admin(admin1).build();
+        Product product2 = Product.builder().name("Second").description("The second product").imageUrl("Url2").date(LocalDate.of(2020, Month.NOVEMBER, 18)).admin(admin2).build();
+        Product product3 = Product.builder().name("Third").description("The third product").imageUrl("Url3").date(LocalDate.of(2021, Month.JANUARY, 1)).admin(admin2).build();
+        productRepository.save(product1);
+        productRepository.save(product2);
+        productRepository.save(product3);
     }
 
 }
