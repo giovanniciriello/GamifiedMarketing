a01bb57080e3987b65086bdbbe00360f573f3b2b
Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
2020-11-18 23:13:29 +0100

    resolved conflict

--- gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/controller/UserController.java
+++ gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/controller/UserController.java
@@ -7,22 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-<<<<<<< HEAD
 import java.util.List;
 
-=======
->>>>>>> dev-giovanni
 @Controller
 public class UserController {
 
     @Autowired
     private UserService userService;
-
-<<<<<<< HEAD
     @Autowired
     private SubmissionService submissionService;
-=======
+
     @GetMapping("/home")
     public String getHomePage() {
         return "home";
@@ -33,23 +27,6 @@ public class UserController {
         return "questionnaire";
     }
 
-    @PostMapping("/add")
-    public String addUser(@RequestParam String first, @RequestParam String last) {
-        User user = new User("Matteo", "Giordano", "email@email.it", "pwd");
-        userRepository.save(user);
-        return "Added new user to repo!";
-    }
->>>>>>> dev-giovanni
-
-    @PostMapping("/addUser")
-    public String addUserRequest(Model model, @RequestParam String firstName, @RequestParam String lastName, @RequestParam String email, @RequestParam String password) {
-        userService.addUser(firstName, lastName, email, password);
-
-        // If we want to pre-compile the email field since the user yet signed up
-        // Then this attribute can be used in Thymeleaf pages with ${email}
-        model.addAttribute("email", email);
-        return "login";
-    }
 
     @GetMapping("/leaderboard")
     public String goToLeaderBoard(Model model){
