2242fcc70616c636590fe0c8b9f55ab44157c8f9
Giovanni Ciriello <giovanni.ciriello.5@gmail.com>
2020-12-16 13:35:44 +0100

    auhtentication system

--- gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/security/WebSecurityConfig.java
+++ gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/security/WebSecurityConfig.java
@@ -1,41 +1,46 @@
 package it.polimi.db2.gamifiedmarketing.application.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select email, password, 1 from users where email = ?")
+                .authoritiesByUsernameQuery("select email, role from users where email = ?");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                    .anyRequest().authenticated()
-                .and()
-                    .formLogin()
-                    .permitAll()
-                    .and()
-                .logout()
-                    .permitAll();
+        http.authorizeRequests()
+                .antMatchers("/leaderboard").hasRole("ADMIN")
+                .antMatchers("/home").hasRole("USER")
+                .antMatchers("/").permitAll()
+                .and().formLogin().loginPage("/login.html");
     }
 
     @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("email")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    public PasswordEncoder getPasswordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
+
+
 }
