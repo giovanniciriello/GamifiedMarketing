6b1c78d70a51ed12675a1bfe01704d9f9a756074
zTehRyaN <MatteoGio96@gmail.com>
2020-12-22 13:47:36 +0100

    (security) Added BCryptPassword Encoder bean

--- gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/security/WebSecurityConfig.java
+++ gamifiedmarketing/src/main/java/it/polimi/db2/gamifiedmarketing/application/security/WebSecurityConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
@@ -38,8 +38,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    public PasswordEncoder getPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
